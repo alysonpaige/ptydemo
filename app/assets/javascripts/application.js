f73fbfa18b6489254be502a4c0335154a7f4f8fd
Alyson Stiffel <alysonstiffel@gmail.com>
2017-10-13 08:56:10 -0400

    bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require turbolinks
+//= require popper
+//= require tether
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require jquery-ui
+//= require bootstrap-sprockets
 //= require_tree .
